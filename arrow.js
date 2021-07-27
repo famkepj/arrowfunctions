8b9fee99257b20b6b0b5161f5d81e1a808600d28
Famke Philips <Famkepj@gmail.com>
2021-07-27 10:17:24 +0200

    arrowfunctions

---
+++ arrow.js
@@ -0,0 +1,22 @@
+/* Deel A */
+  const ikRockArrowFunctions2 = () => console.log("Joe, ik rock de arrow functions!")
+  
+  ikRockArrowFunctions2()
+
+
+/*Deel B*/
+  const fivePlusSeven2 = () => 5 + 7
+
+  fivePlusSeven2();
+
+
+  /* Deel C */
+  const myFunction = (a, b) => a + b
+
+
+  /* Deel D */
+  const addFive = a => a + 5
+
+
+  /* Deel E*/
+const createObject = () => ({greeting: "hoi"});
